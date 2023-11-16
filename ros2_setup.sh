ab816866c44ad3e4cb33137f5175c18064bc08c5
brightenlee <brighten0522@gmail.com>
2023-11-16 18:27:11 +0900

    Update

--- ros2_setup.sh
+++ ros2_setup.sh
@@ -27,8 +27,7 @@
 # NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF
 # THE POSSIBILITY OF SUCH DAMAGE.
 
-helpFunction()
-{
+helpFunction() {
   echo "Usage: ./ros2_setup -r <ros2 distro>"
   echo ""
   echo -e "  -r <ros2 distro>\tfoxy, galactic, humble"
@@ -39,32 +38,30 @@ helpFunction()
 ROS1_DISTRO=""
 ROS2_DISTRO=""
 
-while getopts "r:h" opt
-do
+while getopts "r:h" opt; do
   case "$opt" in
-    r) ROS2_DISTRO="$OPTARG" ;;
-    h) helpFunction ;;
-    ?) helpFunction ;;
+  r) ROS2_DISTRO="$OPTARG" ;;
+  h) helpFunction ;;
+  ?) helpFunction ;;
   esac
 done
 
-if [ "$ROS2_DISTRO" != "foxy" ] && [ "$ROS2_DISTRO" != "galactic" ] && [ "$ROS2_DISTRO" != "humble" ]; then 
+if [ "$ROS2_DISTRO" != "foxy" ] && [ "$ROS2_DISTRO" != "galactic" ] && [ "$ROS2_DISTRO" != "humble" ]; then
   echo -e "\033[1;31mInvalid ROS2 version.\033[0m"
   exit 1
 fi
 
-if [ "$ROS2_DISTRO" == "foxy" ] || [ "$ROS2_DISTRO" != "galactic" ]; then 
+if [ "$ROS2_DISTRO" == "foxy" ] || [ "$ROS2_DISTRO" == "galactic" ]; then
   ROS1_DISTRO="noetic"
 fi
 
-
 # Update repository and install dependencies
-echo -e "\033[1;31mStarting PC setup ...\033[0m"
+echo -e "\033[1;31mUpdate repository and install dependencies.\033[0m"
+
 sudo apt update
 sudo apt upgrade -y
 sudo apt install -y ssh net-tools terminator ntpdate curl vim git
 sudo ntpdate ntp.ubuntu.com
-
 
 # Set locale
 sudo apt update && sudo apt install -y locales
@@ -72,28 +69,24 @@ sudo locale-gen en_US en_US.UTF-8
 sudo update-locale LC_ALL=en_US.UTF-8 LANG=en_US.UTF-8
 export LANG=en_US.UTF-8
 
-
-# Add the ROS2 apt repository
-sudo apt install software-properties-common
-sudo add-apt-repository universe
-sudo curl -sSL https://raw.githubusercontent.com/ros/rosdistro/master/ros.key  -o /usr/share/keyrings/ros-archive-keyring.gpg
-echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/ros-archive-keyring.gpg] http://packages.ros.org/ros2/ubuntu $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/ros2.list > /dev/null
-
-
-# Install development tools and ROS2 tools
-sudo apt install -y python3-flake8-docstrings python3-pip python3-pytest-cov ros-dev-tools
-
 # Install ROS2
-echo -e "\033[1;31mStarting ROS2 $ROS2_DISTRO installation ...\033[0m"
-sudo apt update && sudo apt install -y ros-$ROS2_DISTRO-desktop python3-rosdep ros-$ROS2_DISTRO-rqt* ros-$ROS2_DISTRO-ros2-control ros-$ROS2_DISTRO-ros2-controllers ros-$ROS2_DISTRO-navigation2
+echo -e "\033[1;31mInstall ROS2 $ROS2_DISTRO.\033[0m"
+
+sudo apt install -y software-properties-common
+sudo add-apt-repository universe
+sudo curl -sSL https://raw.githubusercontent.com/ros/rosdistro/master/ros.key -o /usr/share/keyrings/ros-archive-keyring.gpg
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/ros-archive-keyring.gpg] http://packages.ros.org/ros2/ubuntu $(. /etc/os-release && echo $UBUNTU_CODENAME) main" | sudo tee /etc/apt/sources.list.d/ros2.list >/dev/null
+sudo apt update
+sudo apt install -y ros-$ROS2_DISTRO-desktop-full python3-rosdep ros-$ROS2_DISTRO-rqt* ros-$ROS2_DISTRO-ros2-control ros-$ROS2_DISTRO-ros2-controllers ros-$ROS2_DISTRO-navigation2
 
 source /opt/ros/$ROS2_DISTRO/setup.bash
 
 sudo rosdep init
 rosdep update
 
-
 # Create the ROS2 workspace
+echo -e "\033[1;31mCreate the ROS2 workspace.\033[0m"
+
 if [ ! -d "$HOME/colcon_ws" ]; then
   echo -e "\033[1;31mCreating ROS2 workspace ...\033[0m"
   mkdir -p ~/colcon_ws/src
@@ -101,21 +94,22 @@ if [ ! -d "$HOME/colcon_ws" ]; then
   colcon build --symlink-install
 fi
 
-
 # Create the ROS enviornmnet file
+echo -e "\033[1;31mCreate the ROS enviornmnet file.\033[0m"
+
 if [ ! -f "$HOME/env.sh" ] && [ ! -f "/etc/ros/env.sh" ]; then
-  echo -e "\nsource /etc/ros/env.sh" >> ~/.bashrc
+  echo -e "\nsource /etc/ros/env.sh" >>~/.bashrc
 fi
 
 if [ -f "$HOME/env.sh" ]; then
- sudo rm $HOME/env.sh
+  sudo rm $HOME/env.sh
 fi
 
 if [ -f "/etc/ros/env.sh" ]; then
- sudo rm /etc/ros/env.sh
+  sudo rm /etc/ros/env.sh
 fi
 
-if [ "$ROS2_DISTRO" == "foxy" ] || [ "$ROS2_DISTRO" == "galactic" ]; then 
+if [ "$ROS2_DISTRO" == "foxy" ] || [ "$ROS2_DISTRO" == "galactic" ]; then
   echo -e "#!/bin/bash
 
 # Please write the ROS environment variables here
@@ -124,27 +118,21 @@ export ROS_DISTRO=$ROS2_DISTRO
 
 source /opt/ros/\$ROS_DISTRO/setup.bash
 
-if [ \"\$ROS_DISTRO\" = \"$ROS1_DISTRO\" ]; then
-  alias cw='cd ~/catkin_ws'
-  alias cs='cd ~/catkin_ws/src'
-  alias cm='cd ~/catkin_ws && catkin_make'
-
+if [ \"\$ROS_DISTRO\" == \"$ROS1_DISTRO\" ]; then
   source ~/catkin_ws/devel/setup.bash
   export ROS_MASTER_URI=http://localhost:11311
 fi
 
-if [ \"\$ROS_DISTRO\" = \"$ROS2_DISTRO\" ]; then
-  alias cw='cd ~/colcon_ws'
-  alias cs='cd ~/colcon_ws/src'
+if [ \"\$ROS_DISTRO\" == \"$ROS2_DISTRO\" ]; then
   alias cb='cd ~/colcon_ws && colcon build --symlink-install'
 
   source ~/colcon_ws/install/setup.bash
   export RMW_IMPLEMENTATION=rmw_fastrtps_cpp
   export ROS_DOMAIN_ID=101
-fi" >> $HOME/env.sh
+fi" >>$HOME/env.sh
 fi
 
-if [ "$ROS2_DISTRO" == "humble" ]; then 
+if [ "$ROS2_DISTRO" == "humble" ]; then
   echo -e "#!/bin/bash
 
 # Please write the ROS environment variables here
@@ -152,12 +140,10 @@ if [ "$ROS2_DISTRO" == "humble" ]; then
 source /opt/ros/$ROS2_DISTRO/setup.bash
 source ~/colcon_ws/install/setup.bash
 
-alias cw='cd ~/colcon_ws'
-alias cs='cd ~/colcon_ws/src'
 alias cb='cd ~/colcon_ws && colcon build --symlink-install'
   
 export RMW_IMPLEMENTATION=rmw_fastrtps_cpp
-export ROS_DOMAIN_ID=101" >> $HOME/env.sh
+export ROS_DOMAIN_ID=101" >>$HOME/env.sh
 fi
 
 sudo mv $HOME/env.sh /etc/ros/
